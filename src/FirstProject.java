771c5ddd946f8f250137b30ea4cd2e506e8d36bc
Anna Abramova <anna.abramova010498@gmail.com>
2025-05-06 12:26:34 +0100

    new change

--- src/FirstProject.java
+++ src/FirstProject.java
@@ -1,7 +1,9 @@
-public class first_project
+public class FirstProject
 {
     public static void main(String[] args)
     {
         System.out.println("Hello world");
+        System.out.println("Hello world");
+
     }
 }
